90580ff74b5efe4cd191902409c2669da5203521
= <iarlensr@hotmail.com>
2023-03-29 20:14:06 -0300

    commit initial.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-blue-800">
+      <h1 className="text-red-700">Hello Word!</h1>
+    </div>
+  );
+}
+
+export default App;
