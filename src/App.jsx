302bb912b5bcd9fcf144276b1561db74c495e4cc
= <iarlensr@hotmail.com>
2023-03-31 14:41:53 -0300

    aprendendo costumização de classes.

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import React from "react";
 // import WidthHeight from "./components/WidthHeight/WidthHeight";
 // import FlexBox from "./components/FlexBox/FlexBox";
 // import Grid from "./components/Grid/Grid";
-import Responsividade from "./components/Responsividade/Responsividade";
+// import Responsividade from "./components/Responsividade/Responsividade";
+import Constumizacao from "./components/Costumizacao/Constumizacao";
 
 function App() {
   return (
     <div className="p-2 h-screen">
       <h1 className="text-3xl">Aprendendo Tailwind</h1>
-      <Responsividade />
+      <Constumizacao />
     </div>
   );
 }
---
+++ src/components/Costumizacao/Constumizacao.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Constumizacao = () => {
+  return (
+    <div>
+      <h1 className="text-gray-500">Aprendendo classes constumizadas</h1>
+    </div>
+  );
+};
+
+export default Constumizacao;
