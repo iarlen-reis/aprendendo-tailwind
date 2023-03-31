67ea3e6264b16d647ac9d095f5791b405ecd1da5
= <iarlensr@hotmail.com>
2023-03-31 14:20:47 -0300

    aprendendo responsividade.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import React from "react";
 // import Backgrounds from "./components/Backgrounds/Backgrounds";
 // import WidthHeight from "./components/WidthHeight/WidthHeight";
 // import FlexBox from "./components/FlexBox/FlexBox";
-import Grid from "./components/Grid/Grid";
+// import Grid from "./components/Grid/Grid";
+import Responsividade from "./components/Responsividade/Responsividade";
 
 function App() {
   return (
     <div className="p-2 h-screen">
       <h1 className="text-3xl">Aprendendo Tailwind</h1>
-      <Grid />
+      <Responsividade />
     </div>
   );
 }
---
+++ src/components/Responsividade/Responsividade.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Responsividade = () => {
+  return (
+    <div>
+      <h1 className="bg-red-800 sm:bg-green-500 md:bg-cyan-500 xl:bg-yellow-400 2xl:bg-fuchsia-600">
+        Aprendendo responsividade
+      </h1>
+    </div>
+  );
+};
+
+export default Responsividade;
